56212adc55f7b42a752dc180ea974d4c98f8666b
Francesca Santoriello <f.santoriello@hotmail.com>
2023-07-19 10:59:25 +0200

    lecture code

--- shoecommerce/shoe.rb
+++ shoecommerce/shoe.rb
@@ -2,20 +2,58 @@
 ## file_name   singular | lower_case_case
 
 class Shoe
-    # *constructor* > define the properties of the Shoe
     def initialize(size, color)
-        # @size & @color are @instance_vars of the Shoe
         @size  = size
         @color = color
+        @unboxed = false
+    end
+
+    # attr_reader :size, :color
+    # attr_writer :size, :color
+    attr_accessor :size, :color
+
+    def unbox!
+        refund_customer
+
+        @unboxed = true
+    end
+
+    def unboxed?
+        return @unboxed
+    end
+
+    # def color
+    #     return @color
+    # end
+
+    # def size
+    #     return @size
+    # end
+
+    # def color=(new_color)
+    #     @color = new_color
+    # end
+
+    # def size=(new_size)
+    #     @size = new_size
+    # end
+
+    private 
+
+    def refund_customer
+        "I am refunding..."
     end
 end
 
 ### TEST AREA ###
-p my_shoe = Shoe.new(36, "white")
-#<Shoe:0x000000010a3849c8 @size=36, @color="white">
+my_shoe = Shoe.new(36, "wite")
+p my_shoe.refund_customer
 
-p your_shoe = Shoe.new(42, "black")
-#<Shoe:0x00000001064c6470 @size=42, @color="black">
+# my_shoe.unbox!
 
-# Shoe is the *Class*
-# my_shoe & your_shoe are *instances* of Shoe 
+# your_shoe = Shoe.new(42, "black")
+
+# p "This is a #{my_shoe.color} #{my_shoe.size} shoe - #{my_shoe.unboxed?}"
+# p "This is a #{your_shoe.color} #{your_shoe.size} shoe - #{your_shoe.unboxed?}"
+
+# p your_shoe
